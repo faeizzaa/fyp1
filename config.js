755b95dc547536327c45fd3c96e5c4696931803c
Faizah <hjfaeizaa@gmail.com>
2026-06-22 22:21:03 +0800

    update config to render URL

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Change this URL ONCE when you restart ngrok, and it updates across all pages!
-const NGROK_URL = "https://reviver-mocker-revival.ngrok-free.dev";
+const NGROK_URL = "https://fyp1-gnoo.onrender.com";
