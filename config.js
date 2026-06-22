b2c42aa828242ff90a9ab03016d1be45e14af397
Faizah <hjfaeizaa@gmail.com>
2026-06-23 00:16:48 +0800

    update config to render URL

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Change this URL ONCE when you restart ngrok, and it updates across all pages!
-const NGROK_URL = "https://fyp1-gnoo.onrender.com";
+const NGROK_URL = "https://tickago.onrender.com";
