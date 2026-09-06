df121243de17f03170c8193ab0b254c0b127d2e6
Faizah <hjfaeizaa@gmail.com>
2026-09-07 00:50:22 +0800

    bot

--- bot1.py
+++ bot1.py
@@ -3,7 +3,7 @@ from bot_worker import run_single_bot
 TARGET_URL = "https://tickago.onrender.com/waitingroom.html"
 WINDOW_POSITION = (50, 50, 480, 800)
 
-USERNAME = "tier1_bot"
+USERNAME = "abc"
 PASSWORD = "TestPass123!"
 
 if __name__ == "__main__":
